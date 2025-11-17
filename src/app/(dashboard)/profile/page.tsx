4851532df5a27fe940e266016070fd04748e0fe6
karan4533 <mlkaran2004@gmail.com>
2025-11-17 11:04:14 +0530

    commit the proj

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -34,7 +34,7 @@ export default function AddProfilePage() {
               <CardHeader>
                 <CardTitle>Create New Profile</CardTitle>
                 <CardDescription>
-                  Add a new employee profile with all required detail
+                  Add a new employee profile with all required 
                 </CardDescription>
               </CardHeader>
               <CardContent>
